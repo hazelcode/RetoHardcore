9cdbfa44add0b262dc7afc2df2a340d1a77e5e84
Hazel Rojas <lacosaqhabla@gmail.com>
2023-08-14 16:36:15 -0600

    Ahorrar código de RT.cs

--- rt.cs
+++ rt.cs
@@ -13,25 +13,19 @@ namespace RetoHardcore
             }
             return commands;
         }
-        public static Score ReturnSt = new Score("rt.return");
         public Score Return = new Score("rt.return");
-        public static Score corazonFrioSt = new Score("rt.corazonFrio", "used:warped_fungus_on_a_stick");
         public Score corazonFrio = new Score("rt.corazonFrio", "used:warped_fungus_on_a_stick");
-        public static Score corazonFrioCooldownSt = new Score("rt.corazonFrio.Cooldown");
         public Score corazonFrioCooldown = new Score("rt.corazonFrio.Cooldown");
-        public static Score sedSt = new Score("rt.sed");
         public Score sed = new Score("rt.sed");
-        public static Score sedHurtIntervalSt = new Score("rt.sed.hurtInterval");
         public Score sedHurtInterval = new Score("rt.sed.hurtInterval");
-        public static Score dieEventSt = new Score("rt.dieEvent", "deathCount");
         public Score dieEvent = new Score("rt.dieEvent", "deathCount");
         public Score[] scores = {
-            ReturnSt,
-            corazonFrioSt,
-            corazonFrioCooldownSt,
-            sedSt,
-            sedHurtIntervalSt,
-            dieEventSt
+            new Score("rt.return"),
+            new Score("rt.corazonFrio", "used:warped_fungus_on_a_stick"),
+            new Score("rt.corazonFrio.Cooldown"),
+            new Score("rt.sed"),
+            new Score("rt.sed.hurtInterval"),
+            new Score("rt.dieEvent", "deathCount")
         };
     }
 }
