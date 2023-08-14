fa6121d5e1d50b8d661c2ba77159733025b30b4b
Hazel Rojas <lacosaqhabla@gmail.com>
2023-08-14 13:00:21 -0600

    Crear Proyecto

---
+++ RetoHardcore.cs
@@ -0,0 +1,17 @@
+﻿using SteveSharp;
+
+namespace RetoHardcore {
+	public class RetoHardcore {
+		public static void Main(){
+			Project p = new Project(
+				name: "Reto Hardcore",
+				description: "Reto Hardcore by Mikel & LezaH",
+				id: "reto",
+				pack_format: 15,
+				load: "load",
+				main: "main"
+			);
+			Console.ReadLine();
+		}
+	}
+}
